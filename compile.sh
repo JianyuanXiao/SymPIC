7adfc035f290a40d68ca5e6cb5a7ca56e0781401
xiaojianyuan <xiaojy@ustc.edu.cn>
2018-09-16 21:36:14 +0800

    openmp support

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 export CPATH=$CPATH:`pwd`:`pwd`/cgapsio/
 export LIBRARY_PATH=$LIBRARY_PATH:`pwd`:`pwd`/cgapsio/
-for dirs in smallmpi c_ openmp_
+for dirs in cgapsio smallmpi c_ openmp_
 do
 	(cd $dirs ;make -j8;)
 done
